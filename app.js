72c1f47bdbf2120198d00762669a037d7be70c52
BrunoMonteiro92 <bruno.mm1992@gmail.com>
2021-06-04 16:41:41 -0300

    YelpCamp Part 1: Complete CRUD/Modeling DB/Seeding DB
    Signed-off-by: BrunoMonteiro92 <bruno.mm1992@gmail.com>

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+const campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected');
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+	res.render('home');
+});
+
+//Read
+app.get('/campgrounds', async (req, res) => {
+	const campgrounds = await Campground.find({});
+	res.render('campgrounds/index', { campgrounds });
+});
+
+//Create
+app.get('/campgrounds/new', (req, res) => {
+	res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+	const newCamp = new Campground(req.body.campground);
+	await newCamp.save();
+	res.redirect(`/campgrounds/${newCamp._id}`);
+});
+
+//Read by id
+app.get('/campgrounds/:id', async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/show', { campground });
+});
+
+//Update
+app.get('/campgrounds/:id/edit', async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+	const { id } = req.params;
+	const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { runValidators: true, new: true });
+	res.redirect(`/campgrounds/${camp._id}`);
+});
+
+//Delete
+app.delete('/campgrounds/:id', async (req, res) => {
+	const { id } = req.params;
+	const deletedCamp = await Campground.findByIdAndDelete(id);
+	res.redirect('/campgrounds');
+});
+
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
+});
